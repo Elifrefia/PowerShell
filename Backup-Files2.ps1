86a1d5bf77f4b959fbb3eb6d4eb4c71ee80ad899
Elifrefia <48776616+Elifrefia@users.noreply.github.com>
2020-06-11 20:54:45 -0700

    Add files via upload

---
+++ Backup-Files2.ps1
@@ -0,0 +1,21 @@
+﻿Write-Host "Saving the following files" `
+            "to the ""Backup"" folder:"
+$BUCount = 0
+$BUSize = 0
+
+Get-ChildItem -File | Where-Object {
+     $_.LastWriteTime -ge "2000-01-01" } | 
+     ForEach-Object {
+         Write-Host " Backup\$($_.Name)"
+         $BUSize += $_.Length
+         $BUCount ++  
+
+         $FileName = [io.path]::GetFileNameWithoutExtension($_.Name)
+         $FileType = [io.path]::GetExtension( $_.Name)
+
+         $NewFileName = "Backup\$FileName" +"_" + $_.LastWriteTime.ToString("yyy-MM-dd") + $FileType
+
+         Copy-Item $_ $NewFileName }
+
+Write-Host "Saved:" $BUCount "files," `
+                    $BUSize.ToString("N0") "bytes"
